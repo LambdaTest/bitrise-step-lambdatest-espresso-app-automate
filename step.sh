8bd4d52fb71ed60854945226bed2b235b8152bc8
rohithmone-lt <monerohith@lambdatest.com>
2022-07-04 00:07:36 +0530

    Removed basicAuth dependency

--- step.sh
+++ step.sh
@@ -8,8 +8,6 @@ echo "uploading test apk to LambdaTest"
 upload_test_response="$(curl --location --request POST https://$lambdatest_username:$lambdatest_access_key@manual-api.lambdatest.com/app/uploadFramework --form type="espresso-android" --form appFile=@$test_apk_path)"
 test_url=$(echo "$upload_test_response" | jq .app_id)
 
-BASICAUTH=$(echo -n $lambdatest_username:$lambdatest_access_key | base64)
-
 echo "starting automated tests"
 json=$( jq -n \
                 --argjson app_url $app_url \
@@ -25,7 +23,7 @@ json=$( jq -n \
                 --arg network_logs "$lambdatest_network_logs" \
                 --arg video "$lambdatest_video" \
                 '{app: $app_url, testSuite: $test_url, device: $devices, build: $build, queueTimeout: $queue_timeout | tonumber, idleTimeout: $idle_timeout | tonumber, geoLocation: $geo_location, tunnel: $tunnel | test("true"), tunnelIdentifier: $tunnel_identifier, devicelog: $device_logs | test("true"), network: $network_logs | test("true"), video: $video | test("true")}')
-run_test_response="$(curl --location --request POST https://mobile-api.lambdatest.com/framework/v1/espresso/build --header "Authorization: Basic ${BASICAUTH}" --header "Content-Type: application/json" --data-raw \ "$json")"
+run_test_response="$(curl --location --request POST https://$lambdatest_username:$lambdatest_access_key@mobile-api.lambdatest.com/framework/v1/espresso/build --header "Content-Type: application/json" --data-raw \ "$json")"
 build_id=$(echo "$run_test_response" | jq .buildId)
 
 echo "build id: ${build_id}"
